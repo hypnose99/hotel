f81779c8a0cbf06050a0e776abc745b3457f6146
Jonathan <jojo@gmail.com>
2024-12-06 02:13:38 +0000

    Créer modèle utilisateur et de profil personnalisé

--- app/settings.py
+++ app/settings.py
@@ -138,6 +138,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = "userauths.User"
+
+
 JAZZMIN_SETTINGS = {
     'site_header': "Gestion Hôtel",
     'site_brand': "Bienvenue chez Hypnose",
